b6c7dbe505c2c5623e516366c544efe165f3bb70
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-12-20 15:49:36 +0900

    Body영역 추가 및 blind 스타일 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./App.css";
 import Header from "./components/Layout/Header/Header";
+import Aside from "./components/Layout/Aside/Aside";
+import Body from "./components/Layout/Body/Body";
+import "./App.css";
 import "./styles/color.css";
 import "./styles/reset.css";
+import "./styles/blind.css";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Aside></Aside>
+      <main>
+        <Header></Header>
+        <Body></Body>
+      </main>
     </div>
   );
 }
